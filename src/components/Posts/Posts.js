c57ca0369b860435cae073b8455096299eca5493
Nacho Sainz <nacho.sainz@nextiva.com>
2019-02-26 14:13:39 -0600

    add features to add posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,15 +5,29 @@ class Posts extends React.Component {
   state = {
     loading: true,
     posts: this.props.posts || [],
+    title: '',
+    body: '',
   }
 
   componentDidMount() {
     this.props.getPosts();
   }
 
+  onChange = () => {
+    return '';
+  }
+
   render() {
     return (
       <div className="posts">
+      <h1>Posts! :D</h1>
+        <div className="post new">
+        <label htmlFor="">Title</label>
+          <input type="text" value={this.state.title} onChange={this.onChange}/>
+        <label htmlFor="">Body</label>
+          <input type="text" value={this.state.body} onChange={this.onChange}/>
+          <button>Add post</button>
+        </div>
         {this.props.posts.map(post => {
           const { title, body } = post;
           return (
